1a58cdbb17137b3bd9a3a1fb7e9ba63bcc294f7d
Brunopro <brunodanielsantos.ti@gmail.com>
2020-08-30 12:25:59 -0300

    button and input components added

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Image } from 'react-native';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+import logoImg from '../../assets/logo.png';
 
-import { Container } from './styles';
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
-    return <Container />;
+  return (
+    <Container>
+      <Image source={logoImg} />
+      <Title>Faça seu logon</Title>
+      <Input name="email" icon="Mail" placeholder="E-mail" />
+      <Input name="password" icon="Lock" placeholder="Senha" />
+      <Button onPress={() => console.log('Buttton Action')}>Entrar</Button>
+    </Container>
+  );
 };
 
 export default SignIn;
