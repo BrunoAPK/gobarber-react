8e75005553ab662d25a5544f8be20e8abd2fb6eb
Brunopro <brunodanielsantos.ti@gmail.com>
2020-09-01 19:56:46 -0300

    creation of layout of signin and signup pages

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,55 @@
 import React from 'react';
+import {
+  Image,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+import logoImg from '../../assets/logo.png';
 
-import { Container } from './styles';
+import {
+  Container,
+  Title,
+  BackToSignInButton,
+  BackToSignInButtonText,
+} from './styles';
 
-const SignUp: React.FC = () => {
-  return <Container />;
+const SingUp: React.FC = () => {
+  const navigation = useNavigation();
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Criar conta</Title>
+            </View>
+            <Input name="name" icon="user" placeholder="Nome" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+            <Button onPress={() => console.log('Buttton Action')}>Criar</Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <BackToSignInButton onPress={() => navigation.goBack()}>
+        <Icon name="arrow-left" size={20} color="#fff" />
+        <BackToSignInButtonText>Voltar pra logon</BackToSignInButtonText>
+      </BackToSignInButton>
+    </>
+  );
 };
 
-export default SignUp;
+export default SingUp;
